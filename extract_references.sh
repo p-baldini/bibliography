ace799b6ce563486836f23f0c765fea7a3d1f52e
Paolo Baldini <paolo.baldini.phd@gmail.com>
2025-03-25 14:58:29 +0100

    improved references extraction script

--- extract_references.sh
+++ extract_references.sh
@@ -6,7 +6,8 @@ rm $1
 
 echo "Generating new bib file $1 containing all the articles"
 
-bibexport -ns -o $1 *.bib
+BIB_PATH=$(echo $0 | sed 's|/extract_references.sh||')
+bibexport -a -ns -o $1 $(find $BIB_PATH -name "*.bib")
 
 echo "Filtering the bib file $1 maintaining only the cited articles"
 
